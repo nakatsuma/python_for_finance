cf6921792007368b32e57e745bd4012b2be6f561
Teruo Nakatsuma <nakatuma@econ.keio.ac.jp>
2018-06-14 09:51:55 +0900

    Initial commit

---
+++ pyfin_mvf_example2_ver1.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+#%% NumPyの読み込み
+import numpy as np
+#   NumPyのLinalgモジュールの読み込み
+import numpy.linalg as lin
+#   CVXPYの読み込み
+import cvxpy as cvx
+#   MatplotlibのPyplotモジュールの読み込み
+import matplotlib.pyplot as plt
+#   日本語フォントの設定
+from matplotlib.font_manager import FontProperties
+import sys
+if sys.platform.startswith('win'):
+    FontPath = 'C:\\Windows\\Fonts\\meiryo.ttc'
+elif sys.platform.startswith('darwin'):
+    FontPath = '/System/Library/Fonts/ヒラギノ角ゴシック W4.ttc'
+elif sys.platform.startswith('linux'):
+    FontPath = '/usr/share/fonts/truetype/takao-gothic/TakaoExGothic.ttf'
+jpfont = FontProperties(fname=FontPath)
+#%% 最小分散ポートフォリオの計算
+Mu = np.array([1.0, 3.0, 1.5, 6.0, 4.5])
+Stdev = np.array([5.0, 10.0, 7.5, 15.0, 11.0])
+CorrMatrix = np.array([[1.00, 0.25, 0.18, 0.10, 0.25],
+                       [0.25, 1.00, 0.36, 0.20, 0.20],
+                       [0.18, 0.36, 1.00, 0.25, 0.36],
+                       [0.10, 0.20, 0.25, 1.00, 0.45],
+                       [0.25, 0.20, 0.36, 0.45, 1.00]])
+Sigma = np.diag(Stdev).dot(CorrMatrix).dot(np.diag(Stdev))
+iota = np.ones(Mu.shape)
+inv_Sigma = lin.inv(Sigma)
+A = Mu.dot(inv_Sigma).dot(iota)
+B = Mu.dot(inv_Sigma).dot(Mu)
+C = iota.dot(inv_Sigma).dot(iota)
+D = B * C - A ** 2
+#%% 空売り制約の下での分散最小化問題の設定
+Weight = cvx.Variable(Mu.shape[0])
+Target_Return = cvx.Parameter(nonneg=True)
+Risk_Variance = cvx.quad_form(Weight, Sigma)
+Opt_Portfolio = cvx.Problem(cvx.Minimize(Risk_Variance),
+                            [Weight.T*Mu == Target_Return,
+                             cvx.sum(Weight) == 1.0,
+                             Weight >= 0.0])
+#%% 空売り制約の下での最小分散フロンティアの計算
+V_Target = np.linspace(Mu.min(), Mu.max(), num=250)
+V_Risk = np.zeros(V_Target.shape)
+V_Weight = np.zeros((V_Target.shape[0], Mu.shape[0]))
+for idx, Target_Return.value in enumerate(V_Target):
+    Opt_Portfolio.solve()
+    V_Weight[idx, :] = Weight.value.T
+    V_Risk[idx] = np.sqrt(Risk_Variance.value)
+#%% 最小分散フロンティアの作図
+sigma_gmv = 1.0 / np.sqrt(C)
+sigma_p = np.linspace(sigma_gmv, 1.05 * np.max(Stdev), num=250)
+mu_p_efficient = (A + np.sqrt(np.abs(C * sigma_p ** 2 - 1.0) * D)) / C
+fig1 = plt.figure(1, facecolor='w')
+plt.plot(sigma_p, mu_p_efficient, 'k-')
+plt.plot(V_Risk, V_Target, 'k:')
+plt.plot(np.sqrt(np.diagonal(Sigma)), Mu, 'kx')
+plt.legend([u'最小分散フロンティア（空売り制約なし）',
+            u'最小分散フロンティア（空売り制約あり）',
+            u'個別資産'],
+           loc='best',  frameon=False, prop=jpfont)
+plt.xlabel(u'標準偏差(%)', fontproperties=jpfont)
+plt.ylabel(u'期待収益率(%)', fontproperties=jpfont)
+#   投資比率の推移の作図
+fig2 = plt.figure(2, facecolor='w')
+plt.stackplot(V_Target, V_Weight.T*100,
+              colors=tuple([tuple(gray*np.ones(3))
+                            for gray in np.linspace(0.4, 0.8, num=Mu.shape[0])
+                            ]))
+plt.axis([Mu.min(), Mu.max(), 0.0, 100.0])
+plt.legend([u'資産1', u'資産2', u'資産3', u'資産4', u'資産5'],
+           loc='upper left', bbox_to_anchor=(1.0, 1.0),
+           frameon=False, prop=jpfont)
+plt.xlabel(u'目標期待収益率(%)', fontproperties=jpfont)
+plt.ylabel(u'投資比率(%)', fontproperties=jpfont)
+plt.show()
